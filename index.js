54d10565af355f0d9c8098a1cca06c6ad5249f47
unknown <rich.nayan@gmail.com>
2022-10-27 23:43:08 +0600

    api created

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+const topics = require('./data/topics.json')
+
+app.get('/', (req,res)=>{
+    res.send('api running')
+})
+
+app.get('/topic', (req,res)=>{
+    res.send(topics)
+})
+
+app.get('/topic/:id', (req,res)=>{
+    const id = parseInt(req.params.id);
+    console.log(id)
+    const singleData = topics.find(single => single.id == id) || {};
+    res.send(singleData)
+    console.log(singleData)
+})
+
+app.listen(port, ()=>{
+    console.log('server is running on', port)
+})
